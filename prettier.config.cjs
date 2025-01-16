ff935f39dfdf14a94fba4f50fb13279d4de54758
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2025-01-16 15:03:05 +0530

    Ran prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,17 +2,17 @@
 
 /** @type { PrettierConfig | SortImportsConfig } */
 const config = {
-	arrowParens: 'always',
-	printWidth: 80,
-	singleQuote: false,
-	semi: true,
-	trailingComma: 'all',
-	tabWidth: 2,
-	plugins: ['@ianvs/prettier-plugin-sort-imports'],
-	// Last version that doesn't squash type and value imports
-	importOrderTypeScriptVersion: '4.4.0',
-	importOrder: ['<THIRD_PARTY_MODULES>', '', '^~/(.*)$', '^[./]'],
-	proseWrap: 'always', // printWidth line breaks in md/mdx
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  // Last version that doesn't squash type and value imports
+  importOrderTypeScriptVersion: "4.4.0",
+  importOrder: ["<THIRD_PARTY_MODULES>", "", "^~/(.*)$", "^[./]"],
+  proseWrap: "always", // printWidth line breaks in md/mdx
 };
 
 module.exports = config;
--- src/core/executeFunction.ts
+++ src/core/executeFunction.ts
@@ -1,25 +1,25 @@
-import { and, AnyTable, or, SQL } from 'drizzle-orm';
+import { and, AnyTable, or, SQL } from "drizzle-orm";
 
 export const executefunction = (
-	table: AnyTable<any>,
-	functionName: string,
-	args: any[]
+  table: AnyTable<any>,
+  functionName: string,
+  args: any[],
 ): SQL | undefined => {
-	if (!Array.isArray(args)) {
-		throw new Error('args must be an array');
-	}
+  if (!Array.isArray(args)) {
+    throw new Error("args must be an array");
+  }
 
-	if (functionName === 'and') {
-		//@ts-ignore
-		return and(...args);
-	}
-	if (functionName === 'or') {
-		//@ts-ignore
-		return or(...args);
-	}
-	if (functionName === 'eq') {
-		//@ts-ignore
-		return eq(table[args[0]], args[1]);
-	}
-	throw new Error(`Unknown function ${functionName}`);
+  if (functionName === "and") {
+    //@ts-ignore
+    return and(...args);
+  }
+  if (functionName === "or") {
+    //@ts-ignore
+    return or(...args);
+  }
+  if (functionName === "eq") {
+    //@ts-ignore
+    return eq(table[args[0]], args[1]);
+  }
+  throw new Error(`Unknown function ${functionName}`);
 };
